b2a43a40af65e9626761ceed98034d8061ed91b8
Bryan Perdue <bmperdue@gmail.com>
2017-10-24 19:07:03 -0400

    working

--- src/health/meals/components/meal-form/meal-form.component.ts
+++ src/health/meals/components/meal-form/meal-form.component.ts
@@ -1,7 +1,7 @@
-import { Component, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Output, EventEmitter, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormArray, FormGroup, Validator, FormControl, Validators } from '@angular/forms'
 
-import {Meal} from '../../../shared/services/meals/meals.service';
+import { Meal } from '../../../shared/services/meals/meals.service';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -40,20 +40,40 @@ import {Meal} from '../../../shared/services/meals/meals.service';
 
     <div class="meal-form__submit">
       <div>
-        <button class="button" (click)="createMeal()" type="button">Create Meal</button>
+        <button class="button" (click)="createMeal()"  *ngIf="!exists" type="button">Create Meal</button>
+        <button class="button" (click)="updateMeal()" *ngIf="exists" type="button">Update Meal</button>
         <a class="button button--cancel" [routerLink]="['../']">Cancel</a>
       </div>
+
+      <div>
+        <div class="meal-form__delete" *ngIf="exists" >
+          <div *ngIf="toggled">
+            <p>Delete item?</p>
+            <button class="confirm" type="confirm" (click)="removeMeal()">Yes</button>
+            <button class="confirm" type="cancel" (click)="toggle()">No</button>
+          </div>
+          <button class="button button--delete" type="button" (click)="toggle()">Delete</button>
+        </div>
+      </div>
     </div>
-
  </form>
-
 </div>
 `
 })
-export class MealFormComponent {
+export class MealFormComponent implements OnChanges {
+  toggled = false;
+  exists = false;
+  @Input()
+  meal: Meal;
 
   @Output()
   create = new EventEmitter<Meal>();
+
+  @Output()
+  update = new EventEmitter<Meal>();
+
+  @Output()
+  remove = new EventEmitter<Meal>();
 
   form = this.fb.group({
     name: ['', Validators.required],
@@ -62,24 +82,60 @@ export class MealFormComponent {
 
   constructor(private fb: FormBuilder) { }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.meal && this.meal.name) {
+      this.exists = true;
+      const value = this.meal;
+      this.form.patchValue(value);
+
+      this.emptyIngredients();
+
+      if(value.ingredients){
+        for(const item of value.ingredients) {
+          this.ingredients.push(new FormControl(item))
+        }
+      }
+    }
+  }
+
+  emptyIngredients() {
+    while(this.ingredients.controls.length) {
+      this.ingredients.removeAt(0);
+    }
+  }
+
   get ingredients() {
     return this.form.get('ingredients') as FormArray;
   }
 
-  get required(){
+  get required() {
     return this.form.get('name').hasError('required') && this.form.get('name').touched;
   }
 
   createMeal() {
-    if(this.form.valid) {
+    if (this.form.valid) {
       this.create.emit(this.form.value);
     }
   }
 
-  addIngredient(){
+  updateMeal(){
+    if (this.form.valid) {
+      this.update.emit(this.form.value);
+    }
+  }
+
+  removeMeal(){
+    this.remove.emit(this.form.value);
+  }
+
+  addIngredient() {
     this.ingredients.push(new FormControl(''));
   }
   removeIngredient(index: number) {
     this.ingredients.removeAt(index);
+  }
+
+  toggle() {
+    this.toggled = !this.toggled;
   }
 }
--- src/health/shared/services/meals/meals.service.ts
+++ src/health/shared/services/meals/meals.service.ts
@@ -4,6 +4,9 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { AuthService } from '../../../../auth/shared/services/auth/auth.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
 
 export interface Meal {
   name: string;
@@ -30,7 +33,18 @@ export class MealsService {
     return this.db.list(`meals/${this.uid}`).push(meal);
   }
 
+  updateMeal(key: string, meal: Meal) {
+    return this.db.object(`meals/${this.uid}/${key}`).update(meal);
+  }
+
   removeMeal(key: string) {
     return this.db.list(`meals/${this.uid}`).remove(key);
+  }
+
+  getMeal(key: string) {
+    if (!key) return Observable.of({});
+    return this.store.select<Meal[]>('meals')
+      .filter(Boolean)
+      .map(meals => meals.find((meal: Meal) => meal.$key === key));
   }
 }
